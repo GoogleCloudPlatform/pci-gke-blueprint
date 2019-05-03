6a511dd831f8650ff8905e0f9ad53f479d2ccba8
Justin Mound <jmound@google.com>
2019-05-03 15:30:07 -0400

    Deleting in-scope/application and out-of-scope/application (in favor of helm)

--- components/out-of-scope/application/generate-config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -u
-
-# Set vars as needed.
-#LOG_DESTINATION_PROJECT_ID="pci-poc-management"
-sed "s#LOG_DESTINATION_PROJECT_ID#${LOG_DESTINATION_PROJECT_ID}#" templates/fluentd-gcp-config-old-v1.2.5.yaml.tmpl > kubernetes-manifests-system/fluentd-gcp-config-old-v1.2.5.yaml
