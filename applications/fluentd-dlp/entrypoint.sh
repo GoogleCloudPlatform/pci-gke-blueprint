9d67f5d80ef9e2bab2bc245afa2f5cb167b3fdf8
Chris Proto <cproto@google.com>
2019-05-15 12:23:33 -0400

    Add or fix licensing headers
    See issue #1

--- applications/fluentd-dlp/entrypoint.sh
+++ applications/fluentd-dlp/entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 # Copied and edited from:
 # https://github.com/GoogleCloudPlatform/google-fluentd/blob/43b8f932bd4e7e1004c5b8079bd63ead74b1859e/docker/entrypoint.sh
